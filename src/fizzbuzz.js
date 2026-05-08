19243b94dd2568ee4023cc82714068dfc4127920
Antonio Sicardo <rodolfo18p3freney@gmail.com>
2026-05-08 17:47:15 -0600

    se agrego el fizzzbaszbuzzzbadumpst

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,5 +1,18 @@
 export function fizzbuzz(){
-  //tu código va aqui
+  for (let i=1; i<=100; i++){
+    if(i%15==0){
+      console.log("FizzBuzz");
+      
+    }else if(i%5==0){
+      console.log("Buzz");
+
+    }else if(i%3==0){
+      console.log("Fizz");
+
+    }else{
+      console.log(i);
+    }
+  }
  
   
 }
